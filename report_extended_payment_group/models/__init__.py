e27ec9f2490c1620cf64b9719c59acf2fe444503
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-11-18 11:19:14 -0300

    ADD report_extended_payment_group/

---
+++ report_extended_payment_group/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from . import account_payment_group
+from . import report_payment_group
