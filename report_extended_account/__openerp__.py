c832751c7929810c51f38f82161015380d819b12
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-12-31 18:02:50 -0300

    REF

--- report_extended_account/__openerp__.py
+++ report_extended_account/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Report Configurator - Account',
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.1.0',
     'category': 'Reporting Subsystem',
     'sequence': 14,
     'summary': '',
