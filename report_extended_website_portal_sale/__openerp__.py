60ad1be9f9a15ca4c9603bc75cdf8a84b3a01184
nicomacr <nmr@adhoc.com.ar>
2017-08-25 09:41:02 -0300

    FIX report_extended_website_portal_sale

--- report_extended_website_portal_sale/__openerp__.py
+++ report_extended_website_portal_sale/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Report Configurator - Website Portal Sale',
-    'version': '9.0.0.0.0',
+    'version': '9.0.1.0.0',
     'category': 'Reporting Subsystem',
     'sequence': 14,
     'summary': '',
