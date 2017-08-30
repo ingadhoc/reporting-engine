9de16e4f80f1044011ea67958490d883b3480245
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2017-08-30 11:03:05 -0300

    FIX report_extended_account get name funtion (#15)

--- report_extended_account/models/account_invoice.py
+++ report_extended_account/models/account_invoice.py
@@ -33,8 +33,6 @@ class account_invoice(models.Model):
         """
         Para ser usado tmb, por ejemplo, desde qweb en website_portal
         """
-        self = self.with_context(
-            active_model=self._name, active_id=self.id, active_ids=self.ids)
         return self.env['ir.actions.report.xml'].get_report_name(
             self._name, self.ids)
 
@@ -47,6 +45,8 @@ class account_invoice(models.Model):
         self.sent = True
         # if we print caming from other model then active id and active model
         # is wrong and it raise an error with custom filename
+        self = self.with_context(
+            active_model=self._name, active_id=self.id, active_ids=self.ids)
         report_name = self.get_report_name()
         return self.env['report'].get_action(self, report_name)
 
