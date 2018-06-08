a7c9d6aad4c10dfd4e3c6d533e1efef967c8633e
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-06-08 09:17:45 -0300

    [FIX] increase manifest version

--- report_extended_sale/__openerp__.py
+++ report_extended_sale/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Report Configurator - Sale',
-    'version': '9.0.1.1.0',
+    'version': '9.0.1.2.0',
     'category': 'Reporting Subsystem',
     'sequence': 14,
     'summary': '',
