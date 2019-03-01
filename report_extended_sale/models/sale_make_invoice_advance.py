97cc90dd1c5e3bb390243252198b5a1b37a01941
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-03-01 11:57:07 -0300

    [FIX] report_extended_sale: change internal code and notes using parametres insted of fields in companies (#41)

--- report_extended_sale/models/sale_make_invoice_advance.py
+++ report_extended_sale/models/sale_make_invoice_advance.py
@@ -12,8 +12,12 @@ class SaleAdvancePaymentInv(models.TransientModel):
     def _create_invoice(self, order, so_line, amount):
         invoice = super(SaleAdvancePaymentInv, self)._create_invoice(
             order=order, so_line=so_line, amount=amount)
-        if order.company_id.internal_notes:
+        propagate_internal_notes = self.env['ir.config_parameter'].sudo(
+        ).get_param('sale.propagate_internal_notes') == 'True'
+        propagate_note = self.env['ir.config_parameter'].sudo(
+        ).get_param('sale.propagate_note') == 'True'
+        if propagate_internal_notes:
             invoice.internal_notes = order.internal_notes
-        if order.company_id.external_notes:
-            invoice.comment = order.note
+        if not propagate_note:
+            invoice.comment = False
         return invoice
