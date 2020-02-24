0f1a816db6897bb39e44288a636791b0a161657e
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-02-24 16:25:12 -0300

    [REM] google_cloud_print
    deprecate google_cloud_print as this service is going to be deprecated by odoo at end of 2020

--- google_cloud_print/models/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import printing_printer
-from . import google_clodprint_config
-from . import res_users
